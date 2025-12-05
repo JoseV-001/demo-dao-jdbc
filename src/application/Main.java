b965d4da58eecc41f18e2742827cd25eb5389e70
JoseV-001 <Leaphaz@gmail.com>
2025-12-05 18:56:07 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entites.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Bokks");
+
+        System.out.println(obj);
+
+    }
+}
